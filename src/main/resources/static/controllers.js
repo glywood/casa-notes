2b7cafc44846991bf8524d849b35e9d5fd0322a0
Geoff Lywood <glywood@gmail.com>
2015-05-03 01:32:42 -0700

    Working navigation using UI Router, stub pages

---
+++ src/main/resources/static/controllers.js
@@ -0,0 +1,65 @@
+var casaNotesApp = angular.module('casaNotesApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // For any unmatched url, redirect to /chooser
+  $urlRouterProvider.otherwise("/chooser");
+
+  // Now set up the states
+  $stateProvider
+  .state('chooser', {
+    url: "/chooser",
+    views: {
+      "personHeader": { template: "" },
+      "mainView": {
+        templateUrl: "chooser.html",
+        controller: function($scope) {
+          $scope.people = [{"name":"Geoff Lywood", "id":1}];
+        }
+      }
+    }
+  })
+  .state('person', {
+    abstract: true,
+    url: "/person/{personId:int}",
+    views: {
+      "personHeader": { templateUrl: "person_header.html" },
+      "mainView": { template: "<div ui-view></div>" }
+    }
+  })
+  .state('person.overview', {
+    url: "/overview",
+    templateUrl: "overview.html",
+    controller: 'OverviewController'
+  })
+  .state('person.activities', {
+    url: "/activities",
+    templateUrl: "activities.html"
+  })
+  .state('person.activities.new', {
+    url: "/new",
+    templateUrl: "activity.html"
+  })
+  .state('person.reports', {
+    url: "/reports",
+    templateUrl: "reports.html"
+  })
+  .state('person.timeline', {
+    url: "/timeline",
+    templateUrl: "timeline.html"
+  })
+  .state('person.contacts', {
+    url: "/contacts",
+    templateUrl: "contacts.html"
+  })
+  .state('settings', {
+    url: "/settings",
+    views: {
+      "personHeader": { template: "" },
+      "mainView": { templateUrl: "settings.html" }
+    }
+  });
+})
+
+.controller('OverviewController', ['$scope', '$stateParams', function($scope, $stateParams) {
+  $scope.personId = $stateParams.personId;
+}])
